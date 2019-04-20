9e75da11ca8672ba57015f6facd416027efea072
GitGhibli <39234575+GitGhibli@users.noreply.github.com>
2019-04-21 09:16:34 +1200

    blocking reading and writing into/from named pipe

--- NamedPipesCSharp/NamedPipesCSharp/Program.cs
+++ NamedPipesCSharp/NamedPipesCSharp/Program.cs
@@ -9,6 +9,7 @@ namespace NamedPipesCSharp
 {
     class Program
     {
+        //slo by najprv poslat info o tom, co sa bude posielat
         [StructLayout(LayoutKind.Sequential, Pack =1, CharSet = CharSet.Ansi)]
         struct MyStruct
         {
@@ -26,44 +27,38 @@ namespace NamedPipesCSharp
 
         static void Main(string[] args)
         {
-            Task.Factory.StartNew(() =>
+            var task = Task.Factory.StartNew(() =>
             {
                 var server = new NamedPipeServerStream("MyTestPipe", PipeDirection.Out);
-
-                var myStruct = new MyStruct{ MyNumber = 8611, MyAnotherNumber=9027, MyText="Hello World!", MyAnotherText="Hello another World!"};
                 
-                int size = 255 + 255 + 8;
-                byte[] buffer = new byte[size];
-
-                IntPtr memoryPointer = Marshal.AllocHGlobal(size);
-                Marshal.StructureToPtr(myStruct, memoryPointer, true);
-                Marshal.Copy(memoryPointer, buffer, 0, size);
-                Marshal.FreeHGlobal(memoryPointer);
-
-                while (true)
-                {
-                    server.WaitForConnection();
-                    Console.WriteLine(String.Format("New Consumer connected: {0}", DateTime.Now.ToShortTimeString()));
-                    var writer = new BinaryWriter(server);
-                    Thread.Sleep(2000);
-                    writer.Write(buffer);
-                    writer.Flush();
-                    server.Disconnect();
-                }
+                server.WaitForConnection();
+                Console.WriteLine(String.Format("New Consumer connected: {0}", DateTime.Now.ToShortTimeString()));
+                var writer = new BinaryWriter(server);
+                Thread.Sleep(2000);
+                writer.Write(GetByteArray(new MyStruct { MyNumber = 1, MyAnotherNumber = 9027, MyText = "Hello World!", MyAnotherText = "Hello another World!" }));
+                writer.Flush();
+                writer.Write(GetByteArray(new MyStruct { MyNumber = 2, MyAnotherNumber = 9027, MyText = "Hello World!", MyAnotherText = "Hello another World!" }));
+                writer.Flush();
+                writer.Write(GetByteArray(new MyStruct { MyNumber = 3, MyAnotherNumber = 9027, MyText = "Hello World!", MyAnotherText = "Hello another World!" }));
+                writer.Flush();
+                server.Disconnect();
             });
 
-            Task.Delay(1000).Wait();
 
-            //var client = new NamedPipeClientStream("MyTestPipe");
-            //client.Connect();
-            //StreamReader clreader = new StreamReader(client);
-            //StreamWriter clwriter = new StreamWriter(client);
+            task.Wait();
+        }
 
-            string input = Console.ReadLine();
+        static byte[] GetByteArray(MyStruct myStruct)
+        {
+            int size = 255 + 255 + 8;
+            byte[] buffer = new byte[size];
 
-            //clwriter.WriteLine(input);
+            IntPtr memoryPointer = Marshal.AllocHGlobal(size);
+            Marshal.StructureToPtr(myStruct, memoryPointer, true);
+            Marshal.Copy(memoryPointer, buffer, 0, size);
+            Marshal.FreeHGlobal(memoryPointer);
 
-            //clwriter.Flush();
+            return buffer;
         }
     }
 }

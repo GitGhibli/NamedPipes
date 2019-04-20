3c4c48dbcacf01a55188c0fb67e2f67f6fb11cfa
GitGhibli <39234575+GitGhibli@users.noreply.github.com>
2019-04-20 12:56:20 +1200

    example with name pipes const sized struct sending

--- NamedPipesCSharp/NamedPipesCSharp/Program.cs
+++ NamedPipesCSharp/NamedPipesCSharp/Program.cs
@@ -1,26 +1,43 @@
 ﻿using System;
 using System.IO;
 using System.IO.Pipes;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace NamedPipesCSharp
 {
     class Program
     {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        struct MyStruct
+        {
+            public int MyNumber;
+            public int MyAnotherNumber;
+        }
+
         static void Main(string[] args)
         {
             Task.Factory.StartNew(() =>
             {
                 var server = new NamedPipeServerStream("MyTestPipe", PipeDirection.Out);
 
+                var myStruct = new MyStruct{ MyNumber = 8611, MyAnotherNumber=9027};
+                
+                var size = Marshal.SizeOf(myStruct);
+                byte[] buffer = new byte[size];
+
+                IntPtr memoryPointer = Marshal.AllocHGlobal(size);
+                Marshal.StructureToPtr(myStruct, memoryPointer, true);
+                Marshal.Copy(memoryPointer, buffer, 0, size);
+                Marshal.FreeHGlobal(memoryPointer);
+
                 while (true)
                 {
                     server.WaitForConnection();
-                    StreamWriter writer = new StreamWriter(server);
-                    writer.WriteLine("Hello World!");
+                    Console.WriteLine(String.Format("New Consumer connected: {0}", DateTime.Now.ToShortTimeString()));
+                    var writer = new BinaryWriter(server);
+                    writer.Write(buffer);
                     writer.Flush();
-                    //StreamReader reader = new StreamReader(server);
-                    //Console.WriteLine(reader.ReadLine());
                     server.Disconnect();
                 }
             });
